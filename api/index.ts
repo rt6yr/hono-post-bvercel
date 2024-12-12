f0c2819af7af2ecc3545dd2599157a39e44860f8
rt6yr <150139361+rt6yr@users.noreply.github.com>
2024-12-12 15:04:44 +0530

    Initial commit

---
+++ api/index.ts
@@ -0,0 +1,10 @@
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+import app from './routes.js';
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
---
+++ api/routes.ts
@@ -0,0 +1,29 @@
+import { Hono } from 'hono';
+const app = new Hono().basePath('/api');
+
+// Handle GET request
+app.get('/', (c) => {
+  return c.json({ message: "This is a GET request" });
+});
+
+// Handle POST request
+app.post('/', (c) => {
+  return c.json({ message: "This is a POST request" });
+});
+
+// Handle PATCH request
+app.patch('/', (c) => {
+  return c.json({ message: "This is a PATCH request" });
+});
+
+// Handle PUT request
+app.put('/', (c) => {
+  return c.json({ message: "This is a PUT request" });
+});
+
+// Handle OPTIONS request
+app.options('/', (c) => {
+  return c.json({ message: "This is an OPTIONS request" });
+});
+
+export default app;
